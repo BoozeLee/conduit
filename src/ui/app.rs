fab5821a97e3b79558d92b16e0c36c62ff921165
Felipe Coury <felipe.coury@gmail.com>
2026-01-11 14:51:35 -0300

    fix: handles backspace for Set Projects Directory modal

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1400,6 +1400,9 @@ impl App {
                     InputMode::CommandPalette => {
                         self.state.command_palette_state.delete_char();
                     }
+                    InputMode::SettingBaseDir => {
+                        self.state.base_dir_dialog_state.delete_char();
+                    }
                     InputMode::MissingTool => {
                         self.state.missing_tool_dialog_state.backspace();
                     }
@@ -1418,6 +1421,8 @@ impl App {
                     self.state.missing_tool_dialog_state.delete();
                 } else if self.state.input_mode == InputMode::SelectingTheme {
                     self.state.theme_picker_state.delete();
+                } else if self.state.input_mode == InputMode::SettingBaseDir {
+                    self.state.base_dir_dialog_state.delete_forward();
                 } else if let Some(session) = self.state.tab_manager.active_session_mut() {
                     session.input_box.delete();
                 }
